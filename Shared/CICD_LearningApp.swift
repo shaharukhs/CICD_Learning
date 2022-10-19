17d4bc407444d9a4e161c343dc962088f7561d78
Shahrukh Shikalgar <shaharukhs@gmail.com>
2022-10-19 15:54:04 +0530

    first commit

---
+++ Shared/CICD_LearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  CICD_LearningApp.swift
+//  Shared
+//
+//  Created by Shikalgar, Shahrukh on 19/10/22.
+//
+
+import SwiftUI
+
+@main
+struct CICD_LearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Sources/CICD_Learning/CICD_Learning.swift
@@ -0,0 +1,8 @@
+@main
+public struct CICD_Learning {
+    public private(set) var text = "Hello, World!"
+
+    public static func main() {
+        print(CICD_Learning().text)
+    }
+}
